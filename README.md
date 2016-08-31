009d50a30bc97d523958a27fc953b01e0b6cf97d
Andrey Morozov <alt-j@yandex-team.ru>
2016-08-31 20:17:58 +0300

    0.0.13

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # [React] Benchmark for [fast react server](https://github.com/alt-j/fast-react-server)
 
-All test render list component (`./source/list.js`) with data from dataset (`./data.js`).
+| Render Tech \ Children count | 10 | 100 | 1000 |
+--- | --- | --- | ---
+| React + ReactDOMServer  | 14ms | 119ms | 1208ms |
+| React + FastReactRender | 4ms | 36ms | 351ms |
+| FastReactServer + FastReactRender | 1ms | 8ms | 91ms  |
 
 System configuration: Node v4.4.5, MacOS 10.9.5, processor 2GHz Intel Core i7, memory 8Gb 1600MHz DDR3.
 
-For `ITERATION_COUNT = 100` and `CHILDREN_COUNT = 100`:
-- Avarage time of React: 14ms;
-- Avarage time of FastReactServer: 1ms.
-
-For `ITERATION_COUNT = 100` and `CHILDREN_COUNT = 1000`:
-- Avarage time of React: 131ms;
-- Avarage time of FastReactServer: 6ms.
-
-For `ITERATION_COUNT = 100` and `CHILDREN_COUNT = 10000`:
-- Avarage time of React: 1353ms;
-- Avarage time of FastReactServer: 73ms.
+Each test was run 100 times and render list component (`./source/list.js`) with data from dataset (`./data.js`).
 
 # Change configuration
 
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var CHILDREN_COUNT = 100;
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 
+var ReactRender = require('fast-react-render');
+
 var ReactServer = require('fast-react-server');
 
 var dataSet = require('./generate-data')(CHILDREN_COUNT);
@@ -16,13 +18,19 @@ var callback1 = function (listView, dataSet) {
     var element = React.createElement(listView, dataSet);
     return ReactDOMServer.renderToString(element);
 }.bind(this, getListView(React), dataSet);
-console.log('Avarage time of React: ' + test(callback1) + 'ms');
+console.log('Avarage time of React + ReactDOMServer: ' + test(callback1) + 'ms');
 
 var callback2 = function (listView, dataSet) {
+    var element = React.createElement(listView, dataSet);
+    return ReactRender.elementToString(element);
+}.bind(this, getListView(React), dataSet);
+console.log('Avarage time of React + FastReactRender: ' + test(callback2) + 'ms');
+
+var callback3 = function (listView, dataSet) {
     var element = ReactServer.createElement(listView, dataSet);
-    return ReactServer.renderToString(element);
+    return ReactRender.elementToString(element);
 }.bind(this, getListView(ReactServer), dataSet);
-console.log('Avarage time of FastReactServer: ' + test(callback2) + 'ms');
+console.log('Avarage time of FastReactServer + FastReactRender: ' + test(callback3) + 'ms');
 
 function test(callback) {
     var sumTime = 0;
