af4047d90ba74efc51eaeb49e3f033a20a87605c
Igor Stepanov <sigorilla@yandex-team.ru>
2016-09-01 20:14:55 +0300

    Add benchmark.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [React] Benchmark for [fast react server](https://github.com/alt-j/fast-react-server)
 
-| Render Tech \ Children count | 10 | 100 | 1000 |
+| Render Tech \ Children count | 100 | 1000 | 10000 |
 --- | --- | --- | ---
 | React + ReactDOMServer  | 14ms | 119ms | 1208ms |
 | React + FastReactRender | 4ms | 36ms | 351ms |
@@ -8,12 +8,17 @@
 
 System configuration: Node v4.4.5, MacOS 10.9.5, processor 2GHz Intel Core i7, memory 8Gb 1600MHz DDR3.
 
-Each test was run 100 times and render list component (`./source/list.js`) with data from dataset (`./data.js`).
+Each test was render list component (`./source/list.js`) with data from dataset (`./data.js`).
 
 # Change configuration
 
 If you want run benchmark with yours configuration:
-```
+
+```sh
 npm install
 npm run benchmark
 ```
+
+Also you can set your own count of children. You should use for this environment variables:
+
+- `CHILDREN_COUNT` - children count of list (ex. `CHILDREN_COUNT=10 npm run benchmark`)
--- index.js
+++ index.js
@@ -1,45 +1,78 @@
 process.env.NODE_ENV = 'production';
-
-var ITERATION_COUNT = 100;
-var CHILDREN_COUNT = 100;
-
+var Benchmark = require('benchmark');
+var cliff = require('cliff');
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
-
 var ReactRender = require('fast-react-render');
-
 var ReactServer = require('fast-react-server');
 
-var dataSet = require('./generate-data')(CHILDREN_COUNT);
+var childrenCount = process.env.CHILDREN_COUNT || 100;
 
+var dataSet = require('./generate-data')(childrenCount);
 var getListView = require('./source/list');
 
-var callback1 = function (listView, dataSet) {
-    var element = React.createElement(listView, dataSet);
-    return ReactDOMServer.renderToString(element);
-}.bind(this, getListView(React), dataSet);
-console.log('Avarage time of React + ReactDOMServer: ' + test(callback1) + 'ms');
+var tests = {
+    'React + ReactDOMServer': function () {
+        var listView = getListView(React);
+        var element = React.createElement(listView, dataSet);
+        return ReactDOMServer.renderToString(element);
+    },
 
-var callback2 = function (listView, dataSet) {
-    var element = React.createElement(listView, dataSet);
-    return ReactRender.elementToString(element);
-}.bind(this, getListView(React), dataSet);
-console.log('Avarage time of React + FastReactRender: ' + test(callback2) + 'ms');
+    'React + FastReactRender': function () {
+        var listView = getListView(React);
+        var element = React.createElement(listView, dataSet);
+        return ReactRender.elementToString(element);
+    },
 
-var callback3 = function (listView, dataSet) {
-    var element = ReactServer.createElement(listView, dataSet);
-    return ReactRender.elementToString(element);
-}.bind(this, getListView(ReactServer), dataSet);
-console.log('Avarage time of FastReactServer + FastReactRender: ' + test(callback3) + 'ms');
-
-function test(callback) {
-    var sumTime = 0;
-
-    for (var i = 0; i < ITERATION_COUNT; i++) {
-        var start = process.hrtime();
-        callback();
-        sumTime += process.hrtime(start)[0] * 1000 + process.hrtime(start)[1] / 1000000;
+    'FastReactServer + FastReactRender': function () {
+        var listView = getListView(ReactServer);
+        var element = ReactServer.createElement(listView, dataSet);
+        return ReactRender.elementToString(element);
     }
+};
 
-    return (sumTime / ITERATION_COUNT).toFixed(0);
+var results = [];
+var suite = new Benchmark.Suite('comparison', {
+    onStart: function () {
+        console.log('Starts\n');
+    },
+
+    onComplete: function () {
+        console.log('Results for %d children', childrenCount);
+        console.log(cliff.stringifyObjectRows(
+            results,
+            ['name', 'iterations', 'mean time', 'deviation', 'ops/sec'],
+            ['yellow', 'green', 'green', 'purple', 'blue']
+        ));
+        results = [];
+    }
+});
+
+Object.keys(tests).forEach(function (name) {
+    var i = 0;
+    suite.add(name, function () {
+        tests[name]();
+    }, {
+        initCount: 10,
+        onCycle: function () {
+            console.log('\033[1A' + new Array(i++).join('.'));
+        },
+
+        onComplete: function () {
+            console.log('');
+            onTestCompleted.call(this, name);
+        }
+    });
+});
+
+function onTestCompleted(name) {
+    results.push({
+        name: name,
+        iterations: this.count,
+        'mean time': (this.stats.mean * 1000).toFixed(3) + 'ms',
+        deviation: '±' + (this.stats.deviation * 1000).toFixed(4) + 'ms',
+        'ops/sec': (1 / this.stats.mean).toFixed(0)
+    });
 }
+
+suite.run();
